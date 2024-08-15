6e23fb27ba889c6006fee0bc775448ec0a97e48d
joaoleite2 <joao.cont3t@gmail.com>
2024-08-14 22:31:01 -0300

    feat: started project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Page from "./pages";
+
+const App:React.FC = () => {
+  return(
+    <Page />
+  )
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+const Page = () => {
+  return(
+    <section>
+      <ul>
+        <li>joao</li>
+        <li>henrique</li>
+        <li>Leite</li>
+        <li>joao</li>
+        <li>henrique</li>
+        <li>Leite</li>
+        <li>joao</li>
+        <li>henrique</li>
+        <li>Leite</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Page;
