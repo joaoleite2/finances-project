a8f4e5983fa4801c866daf721fc30967915439b0
joaoleite2 <joao.cont3t@gmail.com>
2024-08-15 21:22:00 -0300

    feat: minimize header

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,25 +1,37 @@
-import { ContainerMenu, Group, ItemList, SideMenu } from "./style";
+import { ContainerMenu, Group, ItemList, MinimizeButton, SideMenu } from "./style";
 import dash from "../../assets/dashboard.svg";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { useState } from "react";
+
+const itemMenu = [
+  {
+    icon:dash,
+    text:'Dashboard'
+  },
+];
 
 const SideBar:React.FC = () => {
-  const itemMenu = [
-    {
-      icon:dash,
-      text:'Dashboard'
-    },
-  ]
+  const [itIsOpen, setIsItOpen] = useState(true);
+  
+  const handleOpenClick = () => {
+    setIsItOpen(!itIsOpen);
+  }
   
   return(
-    <SideMenu>
+    <SideMenu className={itIsOpen ? "null" : "closed"}>
       <ContainerMenu>
         <Group>
           {itemMenu.map((item) => (
-            <ItemList><img src={item.icon} /> {item.text}</ItemList>
+            <ItemList><img src={item.icon} /> <p>{item.text}</p></ItemList>
           ))}
         </Group>
         <Group>
-          <ItemList>item.text</ItemList>
+          <ItemList>item</ItemList>
         </Group>
+        {!itIsOpen 
+        ? <MinimizeButton onClick={() => handleOpenClick()}><FaArrowRight /></MinimizeButton> 
+        : <MinimizeButton onClick={() => handleOpenClick()}><FaArrowLeft /></MinimizeButton>
+        }   
       </ContainerMenu>
     </SideMenu>
   )
--- src/components/SideBar/style.ts
+++ src/components/SideBar/style.ts
@@ -4,12 +4,20 @@ export const SideMenu = styled.nav`
   height: 100%;
   background-color: #1e1e1e;
   border-radius: 0 1rem 1rem 0;
+  width: auto;
+
+  &.closed{
+    p{
+      display:none;
+    }
+  }
 `
 
 export const ContainerMenu = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  position: relative;
   height: 100%;
 `
 
@@ -23,14 +31,27 @@ export const ItemList = styled.li`
   align-items: center;
   gap:.5rem;
   color: #fff;
-  width: auto;
-  min-width: 4rem;
   padding:1rem;
   font-weight: 500;
   font-size: .9rem;
   cursor: pointer;
+  justify-content: center;
 
   img{
     width: 1.3rem;
   }
+`
+
+export const MinimizeButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: -1.5rem;
+  bottom:50%;
+  transform: translate(0, 50%);
+  padding: .8rem;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
 `
